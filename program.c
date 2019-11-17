d7cb54b04b0b536691590ad93ba0de9d4b99a8fe
Djuka1 <milanns@live.com>
2019-11-17 13:18:02 +0100

    Update A.1

--- program.c
+++ program.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+int calculate(void);
+void create(void);
+const char * read(const char * path);
+
 int main(void){
 	printf("This is a demo program for merging and branching on git.");	
 	return 0;
