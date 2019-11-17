a9e9c44d8491a050ffc9fd8c961c39a0f2a7cc46
Djuka1 <milanns@live.com>
2019-11-17 13:41:29 +0100

    Update A.2: calculate

--- program.c
+++ program.c
@@ -8,3 +8,12 @@ int main(void){
 	printf("This is a demo program for merging and branching on git.");	
 	return 0;
 }
+
+int calculate(void){
+	// ... function calculates some stuff
+	int result = 0;
+	
+	// ... cue calculations
+	
+	return result;
+}
