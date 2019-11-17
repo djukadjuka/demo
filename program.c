3453282db01e732668e5c6f738479b691c904b1f
Djuka1 <milanns@live.com>
2019-11-17 14:16:16 +0100

    Update A.3

--- program.c
+++ program.c
@@ -4,11 +4,21 @@ int calculate(void);
 void create(void);
 const char * read(const char * path);
 
+
+
 int main(void){
 	printf("This is a demo program for merging and branching on git.");	
+	
+	// TODO: Comment this code ..
+	int x = calculate();
+	create();
+	create();
+	y = x + calculate();
+	
 	return 0;
 }
 
+// This is for calculating things ...
 int calculate(void){
 	// ... function calculates some stuff
 	int result = 0;
@@ -16,7 +26,9 @@ int calculate(void){
 	// ... cue calculations
 	
 	return result;
+}
 
+// This is for creating data in database
 void create(void){
 	// ... function creates some stuff and databases it
 	
